31e1ec79340d694cef088cb5b00df3a27774b213
dkeithdj <42316655+dkeithdj@users.noreply.github.com>
2023-12-31 15:36:38 +0800

    moving frontend files to root

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ require("@solana/wallet-adapter-react-ui/styles.css");
 import "@/app/globals.css";
 import Nav from "@/components/Nav";
 import { WalletConnectProvider } from "@/components/WalletConnectProvider";
-import { Toaster } from "@/components/ui/toaster";
 import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Sanaol",
